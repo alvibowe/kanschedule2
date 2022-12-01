353c0d6d1d34058e9ef65edc439bb2598ec3ab02
davidkamere <davidkamere@live.com>
2022-12-01 16:51:26 +0300

    UI changes for Quotation

--- pages/quotes.js
+++ pages/quotes.js
@@ -283,7 +283,7 @@ const Page = () => {
                     <label htmlFor="fileInput" className="flex flex-col justify-center items-center w-full h-64  rounded-lg border-2 border-blue-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                        {!files.length ? <div className="flex flex-col justify-center items-center">
                             {/* <svg aria-hidden="true" class="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg> */}
-                            <p className="mb-2 text-gray-900 dark:text-gray-400 text-2xl font-bold"><span className="font-semibold ">Click to upload a .xlsx file</span></p>
+                            <p className="mb-2 text-gray-900 dark:text-gray-400 text-xl font-bold"><span className="font-semibold ">Click to upload a .xlsx file</span></p>
                             <p className="text-xs text-gray-500 dark:text-gray-400">.XLSX (MAX. 5MB)</p>
                         </div>:
                         <>
