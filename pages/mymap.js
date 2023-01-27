ef6027b0b3b1a60a88674815d9746e10bc12179e
davidkamere <davidkamere@live.com>
2023-01-27 23:57:31 +0300

    Display my map on My Map technician page

--- pages/mymap.js
+++ pages/mymap.js
@@ -3,6 +3,8 @@ import React, { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import AppLayout from "@lib/components/Layouts/AppLayout";
+// import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
+import Loader from "@lib/components/Loader";
 
 const styles = {
   width: "90vw",
@@ -15,9 +17,24 @@ const Page = () => {
 
     const [map, setMap] = useState(null);
     const mapContainer = useRef(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN
+
+        // const directions = new MapboxDirections({
+        //     accessToken: mapboxgl.accessToken,
+        //     unit: 'metric',
+        //     profile: 'mapbox/driving',
+        //     controls: {
+        //         inputs: false,
+        //         instructions: false,
+        //         profileSwitcher: false
+        //     }
+
+        // })
+
+
         const initializeMap = ({ setMap, mapContainer }) => {
         const map = new mapboxgl.Map({
             container: mapContainer.current,
@@ -27,21 +44,27 @@ const Page = () => {
         });
 
         map.on("load", () => {
+            setLoading(false);
             setMap(map);
             map.resize();
         });
         };
 
-        if (!map) initializeMap({ setMap, mapContainer });
+        if (mapContainer.current && !map) initializeMap({ setMap, mapContainer });
 
-    }, [map])
+    }, [map, mapContainer])
 
 
     return (
         <>
             <AppLayout>
+
+                
+                {/* <div className="m-10 flex justify-center min-h-screen">
+                    <Loader />
+                </div>  */}
                 <div className="m-10 flex justify-center min-h-screen">
-                    <div ref={el => (mapContainer.current = el)} style={styles} />
+                    <div ref={mapContainer} style={styles} />
                 </div>
             </AppLayout>
         </>
